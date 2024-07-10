356bb1d46e6ea12499c4210f5a22cc16255a040a
YUSUF-SELEIM <yusufabdelfattah207@gmail.com>
2024-07-10 20:26:21 +0300

    added authMiddleware before getting todos

--- src/routes.ts
+++ src/routes.ts
@@ -1,9 +1,11 @@
 import { Router } from 'express';
-import { register, login } from './controllers';
+import { register, login, getTodos } from './controllers';
+import { authMiddleware } from './middleware';
 
 const router = Router();
 
 router.post('/register', register);
 router.post('/login', login);
+router.get('/todos', authMiddleware, getTodos);
 
 export default router;
