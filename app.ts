40aee1b097bd5f256c01cf639b6db6954412c00f
YUSUF-SELEIM <yusufabdelfattah207@gmail.com>
2024-07-10 20:21:37 +0300

    used cors to whitelist mu origins and allow credentials

--- app.ts
+++ app.ts
@@ -1,8 +1,15 @@
 import express from 'express';
 import { json } from 'body-parser';
 import routes from './src/routes';
+import cors from 'cors';
 
 const app = express();
+app.use(cors(
+  {
+    origin: ['http://192.168.1.7:8081','http://localhost:8081'],
+    credentials: true
+  }
+));
 app.use(json());
 app.use(routes);
 
